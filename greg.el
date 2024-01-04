ccefe23ce892481a5883be9610b2ceb58a84912e
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2024-01-04 01:06:02 -0500

    bump copyright year

--- greg.el
+++ greg.el
@@ -1,6 +1,6 @@
 ;;; greg.el --- A Text Adventure                     -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2023  Nicholas Vollmer
+;; Copyright (C) 2022-2024  Nicholas Vollmer
 
 ;; Author: Nicholas Vollmer, Daniel Tims
 ;; URL: https://github.com/progfolio/greg
